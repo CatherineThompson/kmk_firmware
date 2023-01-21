d73056526c22dd7b0b288330d562829fbcec5dfc
Catherine Thompson <cathythompson0915@gmail.com>
2023-01-21 11:14:07 -0700

    key mapping changes

--- boards/cathy/lulu/main.py
+++ boards/cathy/lulu/main.py
@@ -1,20 +1,20 @@
 from kb import KMKKeyboard
 from kmk.keys import KC
 from kmk.modules.layers import Layers
-from kmk.modules.modtap import ModTap
 from kmk.modules.split import Split, SplitSide
+from kmk.extensions.media_keys import MediaKeys
 
 keyboard = KMKKeyboard()
-
-modtap = ModTap()
-keyboard.modules.append(modtap)
 
 layers = Layers()
 keyboard.modules.append(layers)
 
+media = MediaKeys()
+keyboard.extensions.append(media)
+
 # TODO Comment one of these on each side
-# split_side = SplitSide.LEFT
-split_side = SplitSide.RIGHT
+split_side = SplitSide.LEFT
+# split_side = SplitSide.RIGHT
 split = Split(
   use_pio=True,
   split_side=split_side,
@@ -23,25 +23,25 @@ keyboard.modules.append(split)
 
 keyboard.keymap = [
     [
-        KC.ESC,  KC.N1,   KC.N2,   KC.N3,    KC.N4,   KC.N5,                    KC.N6,   KC.N7,    KC.N8,   KC.N9, KC.N0,    KC.GRV,
-        KC.TAB,  KC.Q,    KC.W,    KC.E,     KC.R,    KC.T,                     KC.Y,    KC.U,     KC.I,    KC.O,  KC.P,     KC.MINS,
-        KC.LCTL, KC.A,    KC.S,    KC.D,     KC.F,    KC.G,                     KC.H,    KC.J,     KC.K,    KC.L,  KC.SCLN,  KC.QUOT,
-        KC.LSFT, KC.Z,    KC.X,    KC.C,     KC.V,    KC.B,   KC.LBRC, KC.RBRC, KC.N,    KC.M,  KC.COMMA, KC.DOT,  KC.SLSH,  KC.RSFT,
-                                   KC.LGUI, KC.MO(1), KC.LCTL, KC.SPC, KC.ENT,  KC.MO(2), KC.BSPC, KC.RGUI,
+        KC.GRV,   KC.N1,   KC.N2,   KC.N3,    KC.N4,   KC.N5,                    KC.N6,   KC.N7,    KC.N8,    KC.N9,  KC.N0,    KC.EQL,
+        KC.TAB,   KC.Q,    KC.W,    KC.E,     KC.R,    KC.T,                     KC.Y,    KC.U,     KC.I,     KC.O,   KC.P,     KC.MINS,
+        KC.MO(1), KC.A,    KC.S,    KC.D,     KC.F,    KC.G,                     KC.H,    KC.J,     KC.K,     KC.L,   KC.SCLN,  KC.QUOT,
+        KC.LSFT,  KC.Z,    KC.X,    KC.C,     KC.V,    KC.B,    KC.ESC, KC.ENT,  KC.N,    KC.M,     KC.COMMA, KC.DOT, KC.SLSH,  KC.BSLASH,
+                                    KC.LGUI,  KC.LALT, KC.LCTL, KC.ENT, KC.SPC,  KC.BSPC, KC.LBRC,  KC.RBRC,
     ],
     [
-        KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,   KC.F6,                     KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11, KC.F12,
-        KC.TRNS, KC.TRNS, KC.UP,   KC.TRNS, KC.TRNS, KC.TRNS,                   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.EQL, KC.TRNS,
-        KC.TRNS, KC.LEFT, KC.DOWN, KC.RGHT, KC.TRNS, KC.TRNS,                   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
+        KC.F12,  KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,                     KC.F6,   KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,                   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,                   KC.LEFT, KC.DOWN, KC.UP,   KC.RGHT, KC.TRNS, KC.TRNS,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.ESC,  KC.TRNS, KC.TRNS, KC.VOLD, KC.VOLU, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
+                                   KC.TRNS, KC.TRNS, KC.TRNS, KC.MUTE, KC.TRNS, KC.DEL,  KC.TRNS, KC.TRNS,
+    ],
+    [
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,                   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,                   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
+        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,                   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
         KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
-                                   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.DEL,  KC.TRNS,
-    ],
-    [
-        KC.N2,   KC.EXLM, KC.AT,   KC.HASH, KC.DLR,  KC.PERC,                   KC.CIRC, KC.AMPR, KC.ASTR, KC.LPRN, KC.RPRN, KC.TILD,
-        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,                   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.PLUS, KC.UNDS,
-        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,                   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.COLN, KC.DQT,
-        KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.LCBR, KC.RCBR, KC.TRNS, KC.TRNS, KC.LABK, KC.RABK, KC.QUES, KC.TRNS,
-                                   KC.LGUI, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
+                                   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
     ],
     [
         KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,                   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
