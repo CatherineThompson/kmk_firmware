bcbff6fcd9d448f2b3bbe73efc6c7a9ec66b0e87
Catherine Thompson <cathythompson0915@gmail.com>
2023-01-22 23:04:05 -0700

    mappings

--- boards/cathy/lulu/main.py
+++ boards/cathy/lulu/main.py
@@ -23,18 +23,18 @@ keyboard.modules.append(split)
 
 keyboard.keymap = [
     [
-        KC.GRV,   KC.N1,   KC.N2,   KC.N3,    KC.N4,   KC.N5,                    KC.N6,   KC.N7,    KC.N8,    KC.N9,  KC.N0,    KC.EQL,
-        KC.TAB,   KC.Q,    KC.W,    KC.E,     KC.R,    KC.T,                     KC.Y,    KC.U,     KC.I,     KC.O,   KC.P,     KC.MINS,
-        KC.MO(1), KC.A,    KC.S,    KC.D,     KC.F,    KC.G,                     KC.H,    KC.J,     KC.K,     KC.L,   KC.SCLN,  KC.QUOT,
-        KC.LSFT,  KC.Z,    KC.X,    KC.C,     KC.V,    KC.B,    KC.ESC, KC.ENT,  KC.N,    KC.M,     KC.COMMA, KC.DOT, KC.SLSH,  KC.BSLASH,
-                                    KC.LGUI,  KC.LALT, KC.LCTL, KC.DEL, KC.BSPC, KC.SPC, KC.LBRC,  KC.RBRC,
+        KC.GRV,   KC.N1,   KC.N2,   KC.N3,    KC.N4,   KC.N5,                     KC.N6,   KC.N7,    KC.N8,    KC.N9,  KC.N0,    KC.EQL,
+        KC.TAB,   KC.Q,    KC.W,    KC.E,     KC.R,    KC.T,                      KC.Y,    KC.U,     KC.I,     KC.O,   KC.P,     KC.MINS,
+        KC.MO(1), KC.A,    KC.S,    KC.D,     KC.F,    KC.G,                      KC.H,    KC.J,     KC.K,     KC.L,   KC.SCLN,  KC.QUOT,
+        KC.LSFT,  KC.Z,    KC.X,    KC.C,     KC.V,    KC.B,    KC.ESC,  KC.ENT,  KC.N,    KC.M,     KC.COMMA, KC.DOT, KC.SLSH,  KC.BSLASH,
+                                    KC.LGUI,  KC.LALT, KC.LCTL, KC.RSFT, KC.BSPC, KC.SPC, KC.LBRC,  KC.RBRC,
     ],
     [
         KC.F12,  KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,                     KC.F6,   KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11,
         KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,                   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
         KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,                   KC.LEFT, KC.DOWN, KC.UP,   KC.RGHT, KC.TRNS, KC.TRNS,
         KC.TRNS, KC.TRNS, KC.TRNS, KC.ESC,  KC.TRNS, KC.TRNS, KC.VOLD, KC.VOLU, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
-                                   KC.TRNS, KC.TRNS, KC.TRNS, KC.MUTE, KC.TRNS, KC.DEL,  KC.TRNS, KC.TRNS,
+                                   KC.TRNS, KC.TRNS, KC.TRNS, KC.MUTE, KC.DEL,  KC.TRNS,  KC.TRNS, KC.TRNS,
     ],
     [
         KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,                   KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS,
